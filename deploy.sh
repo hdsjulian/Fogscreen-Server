59fe3a84d373e34e3f854a8a4dbb3d08d89f4dd3
Julian Finn <julian@phinn.de>
2026-03-27 14:38:52 +0100

    deploy switching wifi

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -e
 REPO_DIR="/home/raspi/fogscreen-server"
 WEB_DIR="/var/www/html"
 
+# Switch to client mode to reach GitHub
+~/toggle-wifi.sh 0
+
 # Clone if not already present, otherwise pull
 if [ ! -d "$REPO_DIR/.git" ]; then
     git clone https://github.com/hdsjulian/fogscreen-server "$REPO_DIR"
@@ -16,5 +19,8 @@ sudo cp "$REPO_DIR/index.html" "$WEB_DIR/index.html"
 
 # Restart the FastAPI service
 sudo systemctl restart fogscreen
+
+# Switch back to AP mode
+~/toggle-wifi.sh 1
 
 echo "Done."
